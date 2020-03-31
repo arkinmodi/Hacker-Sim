c2d15296f0bb7b27a5283dca96ee42f78ca5b228
huma3 <huma3@mcmaster.ca>
2020-03-31 17:23:06 -0400

    Created SE Attributes Entity

---
+++ Hacker Sim/Assets/Scripts/SEAttributes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEAttributes : MonoBehaviour
+{
+    private string playerName;
+    private string playerGender;
+    private List<(string, int)> playerAttributes;
+    
+    public string PlayerName
+    {
+        get { return playerName; }
+        set { playerName = value; }
+    }
+
+    public string PlayerGender
+    {
+        get { return playerGender; }
+        set { playerGender = value; }
+    }
+
+    public List<(string, int)> PlayerAttributes
+    {
+        get { return playerAttributes; }
+        set { playerAttributes = value; }
+    }
+}
