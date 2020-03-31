3d296929a865abf6f58d3379d8b776f7f3f6585a
huma3 <huma3@mcmaster.ca>
2020-03-31 17:16:29 -0400

    Created User Attribtues entity

---
+++ Hacker Sim/Assets/Scripts/UserAttributes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAttributes
+{
+    private double currency;
+    private double exp;
+    private List<string> friends; // A list containing the usernames of friends
+
+    public double Currency
+    {
+        get { return currency; }
+        set { currency = value; }
+    }
+
+    public double Exp
+    {
+        get { return exp; }
+        set { exp = value; }
+    }
+
+    public List<string> Friends
+    {
+        get { return friends; }
+        set { friends = value; }
+    }
+
+}
