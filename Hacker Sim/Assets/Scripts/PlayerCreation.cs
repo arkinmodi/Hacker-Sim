07382d770740664bdc6f929f1af7a58d490cc7cc
huma3 <huma3@mcmaster.ca>
2020-03-31 18:08:05 -0400

    Started Player Creation

---
+++ Hacker Sim/Assets/Scripts/PlayerCreation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCreation : MonoBehaviour
+{ 
+    private SEAttributes newPlayer;
+    private string playerName;
+    private bool isMale;
+    private bool isFemale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        isMale = GUILayout.Toggle(isMale, "Male");
+        isFemale = GUILayout.Toggle(isFemale, "Female");
+        if (GUILayout.Button("Create"))
+        {
+            if (isMale)
+            {
+                newPlayer.PlayerGender = "Male";
+            } else if (isFemale)
+            {
+                newPlayer.PlayerGender = "Female;"
+            }
+        }
+    }
+}
