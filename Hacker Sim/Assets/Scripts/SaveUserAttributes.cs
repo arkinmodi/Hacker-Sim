ccdcd15e8e04a8fc44684a8ad7b9bb836d12c4cf
huma3 <huma3@mcmaster.ca>
2020-04-04 17:25:35 -0400

    Completed SE Attributes UI

---
+++ Hacker Sim/Assets/Scripts/SaveUserAttributes.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveUserAttributes : MonoBehaviour
+{
+    public static void SavePlayer(UserAttributes user)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string path = Application.persistentDataPath + "/UserAttributes.a";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        UserAttributes saveUser = user;
+
+        formatter.Serialize(stream, saveUser);
+        Debug.Log("Saved file to: " + path);
+        stream.Close();
+    }
+
+    public static UserAttributes LoadUser()
+    {
+        string path = Application.persistentDataPath + "/UserAttributes.a";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            UserAttributes userLoad = formatter.Deserialize(stream) as UserAttributes;
+            stream.Close();
+            return userLoad;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+}
---
+++ Hacker Sim/Assets/Scripts/UpdateSEAttributes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSEAttributes
+{
+
+    public static void IncreaseMood(SEAttributes player) // SE gets happier when using entertainment
+    {
+        player.Mood += 1;
+        SaveSEAttributes.SavePlayer(player);
+    }
+
+    public static void DecreaseMood(SEAttributes player) // SE gets more sad when working
+    {
+        player.Mood -= 1;
+        SaveSEAttributes.SavePlayer(player);
+    }
+
+    public static void IncreaseTired(SEAttributes player) // SE gets more tired over time
+    {
+        player.Tired -= 1;
+        SaveSEAttributes.SavePlayer(player);
+    }
+
+    public static void Rest(SEAttributes player) // SE sleeps and is not tired
+    {
+        player.Tired = 100;
+        SaveSEAttributes.SavePlayer(player);
+    }
+
+    public static void IncreaseHunger(SEAttributes player) // SE gets more hungry over time
+    {
+        player.Hunger -= 1;
+        SaveSEAttributes.SavePlayer(player);
+    }
+
+    public static void DecreaseHunger(SEAttributes player) // When SE eats 
+    {
+        player.Hunger += 1;
+        SaveSEAttributes.SavePlayer(player);
+    }
+}
