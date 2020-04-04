eb56437d9b70bbf6e3406b91a6155a412511ae5b
huma3 <huma3@mcmaster.ca>
2020-04-04 17:38:31 -0400

    Completed SEAttributesUI Version2

---
+++ Hacker Sim/Assets/Scripts/SEAttributesUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SEAttributesUI : MonoBehaviour
+{
+    public SEAttributes player;
+    public GameObject nameText;
+    public GameObject ageText;
+
+    void Start()
+    {
+        player = SaveSEAttributes.LoadPlayer();
+        nameText.GetComponent<Text>().text = player.PlayerName;
+        ageText.GetComponent<Text>().text = "Age: " + player.PlayerAge;
+    }
+}
