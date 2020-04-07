47117264dcfe5bf8dabec1d35dd04c28485b86ab
Christopher Vishnu <vishnuc@mcmaster.ca>
2020-04-07 04:46:01 -0400

    Integration of my part with andrew's along with advancements like exercising and time step

---
+++ HackerSimA&C/Assets/Scripts/TimeStep.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class TimeStep
+{
+    public static int prevtime = 0;
+
+    public static void TimeEvent(int gameMinutes, SEAttributes player)
+    {
+
+
+        if (gameMinutes % 7 == 0 && gameMinutes != prevtime) // In ~ 12 hours (1 day), their hunger, tiredness and fitness will be at 0
+        {
+            SEAttributesController.IncreaseHunger(player);
+            SEAttributesController.IncreaseTired(player);
+            SEAttributesController.DecreaseFitness(player);
+            //UnityEngine.Debug.Log(player.currState);
+            // UnityEngine.Debug.Log(gameMinutes);
+            prevtime = gameMinutes;
+        }
+        if(player.currState != "" && gameMinutes % 4 == 0 && gameMinutes != prevtime)
+        {
+          if (player.currState == "entertainment")
+          {
+              SEAttributesController.IncreaseMood(player);
+          }
+
+          else if (player.currState == "working")
+          {
+              SEAttributesController.DecreaseMood(player);
+              if (gameMinutes % 2 == 0)
+              {
+                  SEAttributesController.IncreaseTired(player);
+              }
+          }
+
+          else if (player.currState == "exercising")
+          {
+              SEAttributesController.IncreaseFitness(player);
+          }
+          prevtime = gameMinutes;
+        }
+    }
+}
---
+++ HackerSimA&C/Assets/Scripts/TimeStepController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeStepController : MonoBehaviour
+{
+    private System.DateTime startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+      startTime = System.DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      SEAttributes player = SaveSEAttributes.LoadPlayer();
+      TimeStep.TimeEvent((System.DateTime.Now - startTime).Seconds, player);
+    }
+}
